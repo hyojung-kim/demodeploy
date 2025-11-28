fcfd75782cb2005b9dbaa1f4a1bb55d76de88f1c
hyo <rlagywnd05@gmail.com>
2025-11-28 18:24:46 +0900

    push2

--- src/main/java/com/back/HomeController.java
+++ src/main/java/com/back/HomeController.java
@@ -9,7 +9,7 @@ public class HomeController {
     public String home() {
         return """
                 <h1>Welcome to the Home Page!</h1>
-                <p>This is a Demo Spring Boot application.</p>
+                <p>멍청한 도커야 !</p>
                 """;
     }
 }
